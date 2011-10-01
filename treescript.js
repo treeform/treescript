cb0ec430bfaff193e749301a121afd35c9d57287
Andre von Houck <starplant@gmail.com>
2011-09-30 22:24:22 -0700

    more work

--- treescript.js
+++ treescript.js
@@ -1,18 +1,25 @@
 var fs = require("fs");
 
+// this translate treescript fns to js operators
+OPS = {
+  // basic math
+  '+':'+','-':'-','*':'*','/':'/','%':'%',
+  // comperison
+  '>':'>','>=':'=>','<':'<','<=':'<=','=':'===','!=':'!==',
+  // function like
+  'inc':'++','dec':'--',
+  // booleans
+  'and':'&&',"or":'||',"not":"!"
+}
+
+var QUOTES_RE = /^['@!~#$]+$/;
 
 // utils
 isString = function(obj) {
     return !!(obj === '' || (obj && obj.charCodeAt && obj.substr));
 };
 
-
-var QUOTES_RE = /^['@!~#$]+$/;
-
 function transform(token) {
-  //if (token.indexOf('"') === 0) {
-  //  return token.substring(1, token.length - 1);
-  //}
   var number = parseFloat(token);
   if (isNaN(number)) {
     return token;
@@ -33,7 +40,6 @@ function tokenize(code) {
       while (true) {
         i++;
         char = code.charAt(i);
-        //console.log("next char", char);
         if(!QUOTES_RE.exec(char)){
           break;
         }
@@ -60,14 +66,13 @@ function tokenize(code) {
       queue = queue + char;
     }
   }
-    //console.log(tokens);
   return tokens;
 };
 
+// turns text into code tree
 function parse(code) {
   var tokens = tokenize(code), parens = 0,
       result = [], stack  = [result], tmp;
-
   for (var i = 0, length = tokens.length, token; token = tokens[i], i < length; i ++) {
     if (token === '(') {
       parens += 1
@@ -95,15 +100,12 @@ function parse(code) {
       stack[0].push(transform(token));
     }
   }
-  //console.log(stack, stack[0].length)
-  //console.log(result);
-  console.log(parens)
   if (parens != 0) throw "unbalanced opening parens"
-
   return result;
 };
 
-
+// to use the buffer add things with
+// out("thing") and remove with rm("thing")
 var buffer = [];
 function out () {
   var args = Array.prototype.slice.call(arguments);
@@ -117,89 +119,119 @@ function rm (what) {
   if (val != what) buffer.push(val);
 }
 
-function compile(err, data) {
-  //parse(data)
+// compile_file
+function compile_file(err, data) {
   var data = parse(data)
   for (var e in data){
-    var exp = data[e];
-    compile_exp(exp);
+    var line = data[e];
+    compile(line);
     out(";\n");
   }
-  code = buffer.join("");
-  console.log(code);
-  //eval(code);
+  // dump the buffer to out
+  console.log(buffer.join(""));
 }
 
-function compile_exp(data) {
+function compile(data) {
   var l = data.length;
-  //console.log(buffer)
   if (isString(data)){
     if (data[0] == '"') {
       out(data);
     } else {
-      out(lookup(data));
+      out(data);
     }
-    return
-  }
-  if (l === undefined){
+  } else if (l === undefined){
+    // if data is not a list, just pass it on
     out(data);
-    return
-  }
-
-  if (data[0] == ":") {
-    out(lookup(data[1]), " = ")
-    compile_exp(data[2])
-    return
+  } else if (data[0] == ":") {
+    // set!
+    out(data[1], " = ")
+    compile(data[2])
   } else if (data[0] == "fn") {
+    // creates a new function
     out("function (", data[1], ") {")
     for(var i = 2; i < data.length; i ++){
       if (i == data.length-1){
         out("return ")
       }
-      compile_exp(data[i])
+      compile(data[i])
       out(";\n")
     }
     out("}")
-    return
+  } else if (data[0] == "let") {
+    // let create new closure via a function
+    out("(function () {\n")
+    out("var ")
+    vars = data[1]
+    for(var i = 0; i < vars.length; i +=2){
+      out(vars[i],"=")
+      compile(vars[i+1])
+      out(", ")
+    }
+    rm(", ")
+    out(";\n")
+    for(var i = 2; i < data.length; i ++){
+      if (i == data.length-1){
+        out("return ")
+      }
+      compile(data[i])
+      out(";\n")
+    }
+    out("})()")
   } else if (data[0] == "js") {
     out(data[1].slice(1,-1))
-    return
   } else if (data[0] == "'") {
     out(JSON.stringify(data.slice(1)))
-    return
-  }
-
-  for (var e in data){
-    var a = data[e];
-    if (e == 0) {
-      //out("call(")
-      compile_exp(a);
-      out("(");
-      //out(",");
-    } else {
-      compile_exp(a);
-      out(",")
+  } else if (OPS[data[0]]) {
+    op = OPS[data[0]]
+    out("(")
+    for(var i = 1; i < data.length; i ++){
+      compile(data[i])
+      out(" ", op, " ")
     }
+    rm(" "); rm(op); rm(" ")
+    out(")")
+  } else if (data[0] == "if") {
+    for(var i = 1; i < data.length; i += 2){
+      if (i == data.length - 1){
+        compile(data[i])
+      } else {
+        compile(data[i])
+        out(" ? ")
+        compile(data[i+1])
+        out(" : ")
+      }
+    }
+    out(";")
+  } else {
+    // function calls
+    for (var e = 0; e < data.length; e ++){
+      var a = data[e];
+      if (e == 0) {
+        if (isString(data[0]) && data[0][0] == "."){
+          out("(")
+          compile(data[1]);
+          out(")")
+          out(data[0], "(");
+          e = 1;
+        } else {
+          compile(a);
+          out("(");
+        }
+      } else {
+        compile(a);
+        out(",")
+      }
+    }
+    rm(",")
+    out(")")
   }
-  rm(",");
-  out(")");
-
 }
 
-function lookup(d){
-  //if (d.match(/^[A-Za-z0-9]*$/)){
-  //return "env['"+d+"']"
-  //}
-  //return d
-  return d.replace(/\+/g,"plus").replace(/\-/g,"__").replace(/\*/g,"mul").replace(/\//g,"div").replace(/\=/g,"eq").replace(/\>/g,"gr").replace(/\</g,"ls").replace(/\'/g,"quote")
-}
-
-//function stub(err, data) {
-//  out(data);
-fs.readFile("cps.tree", 'utf8', compile);
-//}
-
-//fs.readFile("stub.js", 'utf8', stub);
-
+// main
+process.argv.forEach(function (val, index, array) {
+  if (index >= 2) {
+    fs.readFile(val, 'utf8', compile_file);
+  }
+});
 
 
